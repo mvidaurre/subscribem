c0357d79fdc891fbae4d5c15e0f1a9b0ef7cc43e
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2013-05-24 21:28:33 -0500

    Account Sign Up Feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Subscribem::Engine.routes.draw do
+  get '/sign_up', :to => "accounts#new", :as => :sign_up
+  post '/accounts', :to => "accounts#create", :as => :accounts
+  root :to => "dashboard#index"
 end
